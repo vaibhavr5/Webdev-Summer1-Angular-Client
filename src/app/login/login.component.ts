eb65ce3961156a82f78d504d5b1dd084f3af5ab6
vaibhav rangarajan <rvaibhav1793@gmail.com>
2018-06-20 14:18:09 -0400

    Heroku updates

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,8 @@ import {UserServiceClient} from "../services/user.service.client";
 export class LoginComponent implements OnInit {
 
   message: String;
+  username;
+  password;
 credentials;
 user:{};
   login(username, password) {
